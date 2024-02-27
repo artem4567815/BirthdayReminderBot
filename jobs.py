eb8252e1e928aa7f138138eb9f72bed556e090e2
artem <artemiy_08@icloud.com>
2024-02-27 21:31:42 +0300

    hope, final  2 change

--- jobs.py
+++ jobs.py
@@ -2,8 +2,8 @@ from TGBot import sendMessage
 import schedule
 from time import sleep
 
-schedule.every(1).minute.do(sendMessage)
-#schedule.every(1).day.at("07:00").do(sendMessage)
+#schedule.every(1).minute.do(sendMessage)
+schedule.every(1).day.at("07:00").do(sendMessage)
 
 while True:
     schedule.run_pending()
