52f662d06b8741ac83910dbaaffef9e112d7dc0e
roctbb <roctbb@gmail.com>
2024-03-03 13:17:38 +0300

    Update scheduled job timing in jobs.py
    The timing for the scheduled job in jobs.py has been updated to be more dynamic. Rather than being hardcoded at 11:40 daily, it now refers to the variable 'when' from the config file. This allows for easier modifications of the job schedule timing.

--- jobs.py
+++ jobs.py
@@ -1,11 +1,14 @@
 from methods import sendMessage
 import schedule
 from time import sleep
+from config import when
 
-schedule.every(1).day.at("11:40").do(sendMessage)
+print(f"Ready to send at {when}")
+
+schedule.every(1).day.at(when).do(sendMessage)
 
 while True:
     schedule.run_pending()
-    sleep(60)
+    sleep(30)
 
 
